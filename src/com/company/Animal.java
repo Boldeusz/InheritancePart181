694b88d35eeb010feead9f58f0c23fc07a2e9ccd
Boldeusz <mnpower@poczta.onet.pl>
2021-02-01 19:44:00 +0100

    update

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -16,10 +16,10 @@ public class Animal {
         this.weight = weight;
     }
     public void eat(){
-
+        System.out.println("Animal eat() called");
     }
-    public void move(){
-
+    public void move(int speed){
+        System.out.println("Animal is moving at " + speed);
     }
 
     public String getName() {
